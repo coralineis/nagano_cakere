3b9278592c3bc674ee6b864729475b8245855de7
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-22 10:10:47 +0000

    Add Adminモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_095736) do
+ActiveRecord::Schema.define(version: 2022_03_22_100917) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
     t.string "name"
     t.string "postal_code"
     t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "encrypted_password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
