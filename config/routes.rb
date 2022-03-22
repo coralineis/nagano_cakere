1a51f54b410608d252f956f47f03d80670077dce
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-22 12:50:46 +0000

    Add public/itemsコントローラーのルート記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :public do
     root to: 'homes#top'
     get '/about' => 'homes#about'
+    resources :items, only: [:index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
